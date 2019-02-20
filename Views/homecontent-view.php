96b460cff3634d14d4d1fdde6b4a11afbbd81718
rheajuarez84@hotmail.com <rheajuarez84@hotmail.com>
2019-02-20 15:00:16 -0800

    add text and links

--- Views/homecontent-view.php
+++ Views/homecontent-view.php
@@ -15,7 +15,7 @@
 		<div class="blackbox">
 			<h1>About Us</h1>
 			<p>TheBigTree website was made possible by three people that wanted to make a difference for the disabled community.  We know that it can be frustrating to find the right place for your need. Here at TheBigTree, the service is looking for you. All the service you need in one place.</p>
-			<a href="#" id="btn">Learn More</a>
+			<a href="index.php?route=pages.about" id="btn">Learn More</a>
 		</div>
 	</div>
 </section>
